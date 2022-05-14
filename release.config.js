809d2c944815c535593b520987bc155da4a57a39
Zhao Wang <wangzhao2013tsinghua@gmail.com>
2022-05-14 23:37:52 +0800

    ci: semantic-release

---
+++ release.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+    ["@semantic-release/commit-analyzer"],
+    ["@semantic-release/release-notes-generator"],
+    ["@semantic-release/changelog", {changelogFile: "CHANGELOG.md"}],
+    ["@semantic-release/git", {
+      assets: ["CHANGELOG.md"],
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
+  ]
+}
